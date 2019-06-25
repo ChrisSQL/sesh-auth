d626de529643b3e663c280b3510f1c50d57a0392
maher <chrismaher.wit@gmail.com>
2019-06-25 10:36:59 +0100

    Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyBmNkRGMwyD_Z6sPYY3FtKI4H9w7kUzYi4",
-    authDomain: "cohesive-apogee-95023.firebaseapp.com",
-    databaseURL: "https://cohesive-apogee-95023.firebaseio.com",
-    projectId: "cohesive-apogee-95023",
-    storageBucket: "cohesive-apogee-95023.appspot.com",
-    messagingSenderId: "546552200902"
+    apiKey: "AIzaSyCHRLOibxIQtzGaZ7Pi4SFaxFyOqp0H7oU",
+    authDomain: "localhost",
+    databaseURL: "https://tnteventsapp.firebaseio.com/",
+    projectId: "TNTeventsapp",
+    storageBucket: "tnteventsapp.appspot.com",
+    messagingSenderId: "37018668289"
   }
 };
